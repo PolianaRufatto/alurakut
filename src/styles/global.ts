c4dfa2ec2381e20f933b5ee2a20184fe957f9368
Poliana Rufatto <polianarufatto@gmail.com>
2021-07-22 09:22:44 -0300

    Add global styles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,11 +1,44 @@
-import { createGlobalStyle } from 'styled-components';
+import {
+  createGlobalStyle,
+  css,
+  DefaultTheme,
+  GlobalStyleComponent
+} from 'styled-components';
+import { AlurakutStyles } from '../lib/AlurakutCommons';
 
-const GlobalStyles = createGlobalStyle`
+type GlobalStylesProps = {
+  removeBg?: boolean;
+};
+
+const GlobalStyles: GlobalStyleComponent<
+  GlobalStylesProps,
+  DefaultTheme
+> = createGlobalStyle`
   * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
   }
+
+  ${({ theme }) => css`
+    body {
+      background: ${theme.colors.bgPrimary};
+    }
+  `}
+
+  #__next {
+    display: flex;
+    min-height: 100vh;
+    flex-direction: column;
+  }
+
+  img {
+    max-width: 100%;
+    height: auto;
+    display: block;
+  }
+
+  ${AlurakutStyles}
 `;
 
 export default GlobalStyles;
---
+++ src/styles/theme.ts
@@ -0,0 +1,13 @@
+export default {
+  colors: {
+    bgPrimary: '#d9e6f6',
+    bgSecondary: '',
+    bgCard: '#fff'
+  },
+  fonts: {
+    size: '1rem'
+  },
+  shapes: {
+    borderRadius: '8px'
+  }
+};
